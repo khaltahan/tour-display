3c9167728a055cf67c350eb90eb20fe78d6414bf
Khalid <khaltahan@gmail.com>
2022-09-24 13:45:11 -0400

    Toggle button

--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 
 // Main function
 const Tour = ({ id, image, info, price, name }) => {
+  // Declared states
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -11,7 +13,14 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        {/* Only display 200 characters, but if user clicks Read More, display the whole paragraph */}
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button className="delete-btn">not interested</button>
       </footer>
     </article>
   );
