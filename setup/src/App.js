0ccd143da55b31ca48bbde3984ddd889cf801e06
Khalid <khaltahan@gmail.com>
2022-09-24 12:55:03 -0400

    Fetch data

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,11 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+// Required imports
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+// URL to fetch data from
+const url = "https://course-api.com/react-tours-project";
+
+// Main function
 function App() {
-  return <h2>Tours Project Setup</h2>
+  // Declare states
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  // Function to fetch data from URL
+  const fetchTours = async () => {
+    setLoading(true);
+
+    // Try-catch in-case of error fetching data
+    try {
+      // Fetch JSON data
+      const response = await fetch(url);
+      const tours = await response.json();
+      // If data is fetched, set states
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      // If error caught
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  // Perform this function only once immediately after rendering page
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  // If Loading state is true, tell user that data is still being fetched
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  // Else, display the tours rendered
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
-export default App
+export default App;
